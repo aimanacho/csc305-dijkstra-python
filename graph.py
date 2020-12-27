e57a3e858517796015d51a4a3c12ba587d8a8776
wfkhrrzi <wanfakhrulwanazizan15@gmail.com>
2020-12-28 02:32:16 +0800

    second commit

--- graph.py
+++ graph.py
@@ -6,27 +6,30 @@ class Graph:
         self.__node = []
 
     #node1 = origin, node2 = destination
-    def add_unidirectional_edges(self, node1, node2, distance)
+    def add_unidirectional_edges(self, from_node_id, to_node_id, distance):
         # 1. declare two variable nodes
         n1 = n2 = None
         
         # 2. check if exist
         for x in self.__node:
-            if x.getId()==node1:
+            if x.getId()==from_node_id:
                 n1 = x
-            if x.getId()==node2:
+            if x.getId()==to_node_id:
                 n2 = x
 
-        # 3. if n1 or n2 is None, create node1 / node2
+        # 3. if n1 or n2 is None, create from_node_id / to_node_id
         if n1 is None:
-            n1 = Node(node1)
+            n1 = Node(from_node_id)
+            self.__node.append(n1)
+        
         if n2 is None:
-            n2 = Node(node2)
+            n2 = Node(to_node_id)
+            self.__node.append(n2)
 
         # add neighbour
         if n1 in n2.getNeighbors() and n2 in n1.getNeighbors():
             print("Is already neighbours")
-        else
+        else:
             n1.setNeighbor(n2,distance) # n1 ----> n2
 
 
@@ -50,12 +53,17 @@ class Graph:
 
         #Set neighbour between edges
         #Check if 2 edges oredi a neihbor exist
-        if n1 in n2.getNeighbors() and n2 in n1.getNeighbors()
+        if n1 in n2.getNeighbors() and n2 in n1.getNeighbors():
             print(n1.getId()+" and "+n2.getId()+" already a neighbour.")
-        else
+        else:
             n1.setNeighbor(n2,distance) # n1 ----> n2
             n2.setNeighbor(n1,distance) # n2 ----> n1
 
+    def displayNode(self):
+        for x in self.__node:
+            print(x)
+
+        
             
         
 
